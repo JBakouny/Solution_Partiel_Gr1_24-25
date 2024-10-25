4a6010ea50a77bd309cb3c2e2b0823a090fbefc2
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2024-10-25 18:11:36 +0300

    TODO: Implement the code of ex_2 using printf and scanf instead of cout and cin

--- Ex_2/ex_2.cpp
+++ Ex_2/ex_2.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <cstdio> // TODO replace iostream by cstdio
+// cplusplus.com -> documentation printf et scanf
 using namespace std;
 
 void inputArray(int size, int arr[]) {
     for (int i = 0; i < size; ++i)
     {
         cout << "Entrez la valeur " << i+1 << " : ";
-        cin >> arr[i];
+        cin >> *(arr + i);
     }
 }
 
@@ -13,18 +15,33 @@ void displayArray(int size, int arr[]) {
     cout << "[ ";
     for (int i = 0; i < size; ++i)
     {
-        cout << arr[i] << " ";
+        cout << *(arr + i) << " ";
     }
     cout << "]";
 }
 
 int main() {
-    const int size = 5;
+    //const int size = 5;
 
-    int array[size];
-    inputArray(size, array);
-    cout << "Tableau original : ";
-    cout << endl;
-    displayArray(size, array);
+    //int array[size];
+    //inputArray(size, array);
+    //cout << "Tableau original : ";
+    //cout << endl;
+    //displayArray(size, array);
+
+    int x = 10;
+
+    cout << x << endl;
+    
+    int* p;
+    p = &x;
+    
+    cout << *p << endl;
+
+    *p = 15;
+    cout << x << endl;
+
+
+
     return 0;
 }
